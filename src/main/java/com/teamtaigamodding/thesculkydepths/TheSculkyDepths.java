ae5924c77c971e5f20e2d167c895623e54a424f6
TheLegitDolt <asphaltpotatofy@gmail.com>
2022-08-13 00:27:31 -0700

    initial commit

---
+++ src/main/java/com/teamtaigamodding/thesculkydepths/TheSculkyDepths.java
@@ -0,0 +1,17 @@
+package com.teamtaigamodding.thesculkydepths;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TheSculkyDepths.MODID)
+public class TheSculkyDepths {
+    public static final String MODID = "the_sculky_depths";
+
+    public TheSculkyDepths() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
