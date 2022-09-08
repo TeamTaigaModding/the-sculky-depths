a61254d7557e436be829e8c0b9db1563606e22bf
Carmenboy <76805533+Carmenboy1@users.noreply.github.com>
2022-09-09 10:00:25 +1200

    Deepslate Lantern i think

--- src/main/java/com/teamtaigamodding/thesculkydepths/common/blocks/BlockProperties.java
+++ src/main/java/com/teamtaigamodding/thesculkydepths/common/blocks/BlockProperties.java
@@ -7,5 +7,7 @@ import net.minecraft.world.level.material.MaterialColor;
 
 public class BlockProperties {
     public static final BlockBehaviour.Properties ECHO_CRYSTAL = BlockBehaviour.Properties.of(Material.SCULK, MaterialColor.COLOR_CYAN).strength(1F).sound(SoundType.SCULK_SENSOR);
+    public static final BlockBehaviour.Properties DEEPSLATE_LANTERN = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).strength(3.5F).requiresCorrectToolForDrops().
+            lightLevel(state -> 10).sound(SoundType.DEEPSLATE_BRICKS)), CreativeModeTab.TAB_BUILDING_BLOCKS);
 
 }
