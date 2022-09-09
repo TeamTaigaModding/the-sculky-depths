42b9b4e3fd283780f87f4a106ddbc2087f8c72cb
Carmenboy <76805533+Carmenboy1@users.noreply.github.com>
2022-09-09 14:10:57 +1200

    Change EchoCrystalBlock to Block class (im a silly billy)

--- src/main/java/com/teamtaigamodding/thesculkydepths/common/registry/TSDBlocks.java
+++ src/main/java/com/teamtaigamodding/thesculkydepths/common/registry/TSDBlocks.java
@@ -16,5 +16,5 @@ public class TSDBlocks {
 
     public static final RegistryObject<Block> ECHO_CRYSTAL = HELPER.createBlock("echo_crystal", () -> new EchoCrystalBlock(BlockProperties.ECHO_CRYSTAL), CreativeModeTab.TAB_DECORATIONS);
     public static final RegistryObject<Block> STONE_CHEST = HELPER.createBlock("stone_chest", () -> new StoneChestBlock("stone", BlockProperties.STONE_CHEST), CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> DEEPSLATE_LANTERN = HELPER.createBlock("deepslate_lantern", () -> new EchoCrystalBlock(BlockProperties.DEEPSLATE_LANTERN), CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> DEEPSLATE_LANTERN = HELPER.createBlock("deepslate_lantern", () -> new Block(BlockProperties.DEEPSLATE_LANTERN), CreativeModeTab.TAB_BUILDING_BLOCKS);
 }
