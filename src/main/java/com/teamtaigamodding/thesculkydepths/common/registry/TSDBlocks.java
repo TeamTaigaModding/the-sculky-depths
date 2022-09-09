0f113d3d8e1863a09764cc0ce25d5c817ebfdb52
TheLegitDolt <asphaltpotatofy@gmail.com>
2022-09-08 20:32:17 -0700

    on more fix before homework

--- src/main/java/com/teamtaigamodding/thesculkydepths/common/registry/TSDBlocks.java
+++ src/main/java/com/teamtaigamodding/thesculkydepths/common/registry/TSDBlocks.java
@@ -16,7 +16,7 @@ public class TSDBlocks {
     public static final BlockSubRegistryHelper HELPER = TheSculkyDepths.REGISTRY_HELPER.getBlockSubHelper();
 
     public static final RegistryObject<Block> ECHO_CRYSTAL = HELPER.createBlock("echo_crystal", () -> new EchoCrystalBlock(BlockProperties.ECHO_CRYSTAL), CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> STONE_CHEST = HELPER.createBlock("stone_chest", () -> new StoneChestBlock("stone", BlockProperties.STONE_CHEST), CreativeModeTab.TAB_DECORATIONS);
+    public static final RegistryObject<Block> STONE_CHEST = HELPER.createBlock("stone_chest", () -> new StoneChestBlock( BlockProperties.STONE_CHEST), CreativeModeTab.TAB_DECORATIONS);
     public static final RegistryObject<Block> DEEPSLATE_LANTERN = HELPER.createBlock("deepslate_lantern", () -> new Block(BlockProperties.DEEPSLATE_LANTERN), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> SCULK_BONE_BLOCK = HELPER.createBlock("sculk_bone_block", () -> new RotatedPillarBlock(BlockProperties.SCULK_BONE_BLOCK), CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> CARVED_SCULK_BONE_BLOCK = HELPER.createBlock("carved_sculk_bone_block", () -> new RotatedPillarBlock(BlockProperties.CARVED_SCULK_BONE_BLOCK), CreativeModeTab.TAB_BUILDING_BLOCKS);
