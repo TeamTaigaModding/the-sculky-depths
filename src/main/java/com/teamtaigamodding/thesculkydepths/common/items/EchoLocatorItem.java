a7d4f2fc7536f668b49143b5fd61ac402874cf5f
Carmenboy <76805533+Carmenboy1@users.noreply.github.com>
2022-09-09 21:11:45 +1200

    Wip Echolocator Things

---
+++ src/main/java/com/teamtaigamodding/thesculkydepths/common/items/EchoLocatorItem.java
@@ -0,0 +1,12 @@
+package com.teamtaigamodding.thesculkydepths.common.items;
+
+import net.minecraft.world.item.SpyglassItem;
+
+public class EchoLocatorItem extends SpyglassItem {
+
+    public EchoLocatorItem(Properties properties) {
+        super(properties);
+    }
+
+}
+
--- src/main/java/com/teamtaigamodding/thesculkydepths/common/registry/TSDItems.java
+++ src/main/java/com/teamtaigamodding/thesculkydepths/common/registry/TSDItems.java
@@ -1,13 +1,19 @@
 package com.teamtaigamodding.thesculkydepths.common.registry;
 
 import com.teamabnormals.blueprint.core.util.registry.BlockSubRegistryHelper;
+import com.teamabnormals.blueprint.core.util.registry.ItemSubRegistryHelper;
 import com.teamtaigamodding.thesculkydepths.TheSculkyDepths;
 import com.teamtaigamodding.thesculkydepths.common.blocks.BlockProperties;
 import com.teamtaigamodding.thesculkydepths.common.blocks.EchoCrystalBlock;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(modid = TheSculkyDepths.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class TSDItems { }
+public class TSDItems {
+    public static final ItemSubRegistryHelper HELPER = TheSculkyDepths.REGISTRY_HELPER.getItemSubHelper();
+
+    public static final RegistryObject<Item> ECHOLOCATOR = HELPER.createItem("echolocator", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+}
