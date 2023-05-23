3e0b6f8c88ef8410e43ab21274d4f21cbc147274
Carmenboy <76805533+Carmenboy1@users.noreply.github.com>
2023-05-24 08:13:30 +1200

    Removed Carved Sculk Bone

--- src/main/java/com/teamtaigamodding/thesculkydepths/common/blocks/TSDBlockProps.java
+++ src/main/java/com/teamtaigamodding/thesculkydepths/common/blocks/TSDBlockProps.java
@@ -11,7 +11,6 @@ public class TSDBlockProps {
     public static final BlockBehaviour.Properties ECHO_CRYSTAL = BlockBehaviour.Properties.of(Material.SCULK, MaterialColor.COLOR_CYAN).strength(1F).sound(TSDSoundTypes.ECHO_CRYSTAL);
     public static final BlockBehaviour.Properties DEEPSLATE_LANTERN = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).strength(3.5F).lightLevel(state -> 10).sound(SoundType.DEEPSLATE_BRICKS);
     public static final BlockBehaviour.Properties SCULK_CARTILAGE = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GRAY).strength(2F).sound(SoundType.SCULK_CATALYST);
-    public static final BlockBehaviour.Properties CARVED_SCULK_BONE_BLOCK = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GRAY).strength(2F).sound(SoundType.BONE_BLOCK);
     public static final BlockBehaviour.Properties STONE_CHEST = BlockBehaviour.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).strength(5F).sound(SoundType.DEEPSLATE_BRICKS).noOcclusion();
     public static final BlockBehaviour.Properties SCULK_VERTEBRAE = BlockBehaviour.Properties.of(Material.SCULK, MaterialColor.COLOR_BLACK).strength(0.4F).sound(SoundType.SCULK_SHRIEKER);
     public static final BlockBehaviour.Properties BROKEN_JUKEBOX = BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.COLOR_BLACK).strength(2.0F, 6.0F).sound(SoundType.STONE);
